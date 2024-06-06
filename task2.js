33d133aced138d16f5f80cf6da56ff3eb7031333
Thofikh Bisyron <162830476+ThofikhBisyron@users.noreply.github.com>
2024-06-06 23:26:20 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,3 +1,5 @@
+const searchName = function (keyword, limit, cb){
+
 const name = [
     "Abigail",
     "Alexandra",
@@ -15,21 +17,67 @@ const name = [
     "Faith",
     "Penolope",
   ];
-  function searchName(parameter,limit,callback){
-    let result = [];
-    for (i = 0; i < name.length; i++){
-      if (name[i].toLowerCase().indexOf(parameter.toLowerCase()) > -1){
-        result.push(name[i]);
-      if (result.length == limit){
-        break;
-      }
-    }
-  }
-    callback(result)
-  }
+  const up =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const low = "abcdefghijklmnopqrstuvwxyz"
+  let searchResult = []
 
-  function printNama(result){
-    console.log(result);
-  }
-  searchName("an", 3, printNama)
+  for (let i=0; i < name.length; i++){
+    const personName = name[i]
+    let result = ""
+
+    for(let i = 0; i < personName.length; i++ ){
+        let converted = false
+        for(let j=0; j < up.length; j++){
+            if (converted === false){
+                if(personName[i] === up[j]){
+                    result += low[j]
+                    converted = true
+                }
+                if (j === 25){
+                    if (personName[i] !== up[j]){
+                        result += personName[i]
+                        converted =true
+                    }
+                }
+            }
+        }
+    }
+ 
+
+  let isfound = false
+  for (let i = 0; i < result.length; i++){
+    let segment = ""
+    for (let j=i; j < keyword.length + i; j++){
+        if (result[j] !== undefined){
+            segment += result[j]
+        }
+        }
+        if(segment === keyword){
+        isfound = true
+    }
+}
+    if (isfound === true){
+        searchResult = [...searchResult, personName]
+    }
+}
+    
+    cb(searchResult, limit)
+}
+    
+
+    function limitResult (arr, lim){
+        let result = []
+        for (let i = 0; i<lim; i++){
+            if(arr[i]){
+                result = [...result, arr[i]]
+            }  
+        }
+        console.log(result)
+    }
+
+
+searchName ("an", 3, limitResult)
+
+
+
 
